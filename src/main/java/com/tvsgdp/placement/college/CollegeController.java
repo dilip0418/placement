9403f82656d7aa20a9d8b2b0721393c3b48f74ba
dilip0418 <sudheer0418@gmail.com>
2023-09-30 02:17:08 +0530

    CollegeController commit (unknown)

--- src/main/java/com/tvsgdp/placement/college/CollegeController.java
+++ src/main/java/com/tvsgdp/placement/college/CollegeController.java
@@ -1,11 +1,14 @@
 package com.tvsgdp.placement.college;
 
+import com.tvsgdp.placement.exception.NoCollegeFoundWithLocationException;
 import com.tvsgdp.placement.exception.UserAlreadyHasACollegeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -15,42 +18,79 @@ public class CollegeController {
 
     private final CollegeService collegeService;
 
-    @GetMapping("/{collegeAdminId}")
-    public ResponseEntity<?> getCollegeByCollegeAdminId(@PathVariable Long collegeAdminId) {
-        Optional<?> response = collegeService.getCollegeByCollegeAdminId(collegeAdminId);
+    //get college by college Id
+    @GetMapping("/{id}")
+    public ResponseEntity<CollegeResponse> getCollegeById(@PathVariable Long id) {
+        Optional<CollegeResponse> response = collegeService.getCollegeById(id);
         if (response.isEmpty()) {
-            return new ResponseEntity<>("No college found with this admin id", HttpStatus.NOT_FOUND);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ResponseEntity.of(response);
     }
 
+    //get college by college admin Id
+    @GetMapping("/college-admin/{collegeAdminId}")
+    public ResponseEntity<CollegeResponse> getCollegeByCollegeAdminId(@PathVariable Long collegeAdminId) {
+        Optional<CollegeResponse> response = collegeService.getCollegeByCollegeAdminId(collegeAdminId);
+        if (response.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(response);
+    }
+
+    //get college by location
     @GetMapping("/location/{location}")
-    public ResponseEntity<?> getAllCollegeLocation(@PathVariable String location) {
-        Optional<?> response = collegeService.getAllCollegeLocation(location);
-        if (response.isEmpty()) {
-            return new ResponseEntity<>("No college found with this location", HttpStatus.NOT_FOUND);
+    @PreAuthorize("hasRole('ROLE_CORPORATE')")
+    public ResponseEntity<List<CollegeResponse>> getAllCollegeLocation(@PathVariable String location) {
+        List<CollegeResponse> response;
+        try {
+            response = collegeService.getAllCollegeLocation(location);
+        } catch (NoCollegeFoundWithLocationException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-    @GetMapping("/all")
-    public ResponseEntity<?> getAllCollege() {
-        Optional<?> response = collegeService.getAllCollege();
-        if (response.isEmpty()) {
-            return new ResponseEntity<>("No college found", HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ResponseEntity.ok(response);
     }
 
+    //get all college
+    @GetMapping("/all")
+    @PreAuthorize("hasRole('ROLE_CORPORATE')")
+    public ResponseEntity<List<CollegeResponse>> getAllCollege() {
+        List<CollegeResponse> response = collegeService.getAllCollege();
+        if (response.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    // create a new college - [Note one uer can create only one college]
     @PostMapping("/create")
-    public ResponseEntity<?> createCollege(@RequestBody CollegeRequest collegeRequest) {
-        // Handle the response of the POST request by using the wild card ? parameter as the return type
+    @PreAuthorize("hasRole('ROLE_UNIVERSITY')")
+    public ResponseEntity<CollegeResponse> createCollege(@RequestBody CollegeRequest collegeRequest) {
+
         CollegeResponse response = null;
         try {
             response = collegeService.createCollege(collegeRequest);
         } catch (UserAlreadyHasACollegeException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         System.out.println(response.toString());
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    //update a college
+    @PutMapping("/update")
+    @PreAuthorize("hasRole('ROLE_UNIVERSITY')")
+    public ResponseEntity<CollegeResponse> updateCollege(@RequestBody CollegeRequest collegeRequest) {
+        CollegeResponse response = null;
+        try {
+            response = collegeService.updateCollege(collegeRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        System.out.println(response.toString());
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
